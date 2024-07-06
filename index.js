3ab23deef91ff2b7fb1bbfcd0d7458853a5febf4
unknown <mendibass@gmail.com>
2024-07-07 00:38:06 +0300

    remove 'express-async-errors'

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('express-async-errors');
+// require('express-async-errors');
 const cors = require('cors');
 const app = express();
 const helmet = require('helmet');
